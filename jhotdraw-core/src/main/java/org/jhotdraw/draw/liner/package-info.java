8f0d22e472e4f8b205902e52c57d33b6a5df8c11
Mark Li <markl6943@gmail.com>
2022-11-24 22:35:14 -0500

    first commit

---
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/liner/package-info.java
@@ -0,0 +1,19 @@
+/* @(#)package-info.java
+ *
+ * Copyright (c) 1996-2010 The authors and contributors of JHotDraw.
+ * You may not use, copy or modify this file, except in compliance with the
+ * accompanying license terms.
+ *
+ * @author Werner Randelshofer
+ * @version $Id$
+*/
+
+/**
+Provides interfaces and classes for laying out the path of a
+{@link org.jhotdraw.draw.BezierFigure}.
+*/
+@DefaultAnnotation(Nonnull.class)
+package org.jhotdraw.draw.liner;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import javax.annotation.Nonnull;
